3945ca1dbc71c5a2e0be5b6965c490159ed3f7ee
binit Kumar Ambastha <binit.ambastha@gmail.com>
2018-02-25 07:34:34 +0000

    assignment 1st

---
+++ Assignment1.sh
@@ -0,0 +1,13 @@
+echo $n
+for i in $@
+do 
+	sum=`expr $sum + $i`
+	if (("$i" > 10))
+	then
+		echo $i
+		a=`expr $a + $i`
+	fi
+done
+echo "sum of "$#" no is : "$sum
+echo "sum of numbers greater than 10 is : "$a
+
